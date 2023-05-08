8f8b279c9517d10512a2694f2cfe0482407e5e36
gremble0 <hermanstornes@gmail.com>
2023-05-08 11:29:10 +0200

    Added laptop setup for polybar

--- .config/polybar/laptop.sh
+++ .config/polybar/laptop.sh
@@ -1,14 +1,4 @@
 # kill old instances
 killall polybar
 
-# my machine only
 polybar --reload laptop &
-
-# general purpose
-#if type "xrandr"; then
-#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#    MONITOR=$m polybar --reload main &
-#  done
-#else
-#  polybar --reload main &
-#fi
