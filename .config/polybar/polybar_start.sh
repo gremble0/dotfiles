89b24cd7bd215d6f2287b699db08fb7c4955713b
gremble0 <hermanstornes@gmail.com>
2023-05-12 19:18:21 +0200

    Fixed polybar start script

--- .config/polybar/polybar_start.sh
+++ .config/polybar/polybar_start.sh
@@ -1,8 +1,8 @@
 # kill old instances
 killall polybar
 
-# If there are multiple displays connected load 2 polybar instances, else one
-if $(xrandr | grep ' connected' | wc -l) > 1; then
+# If there are multiple displays connected load 2 polybar instances, if not one
+if [ $(xrandr | grep -c ' connected') -gt 1 ]; then
     polybar --reload main &
     polybar --reload second &
 else 
