6622816ef49acbfdca6efc38aad58d2d5b8add37
gremble0 <hermanstornes@gmail.com>
2023-07-16 04:13:17 +0200

    Added borders for lsp hover event, added shorthand variable

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,7 @@
 -- [[ Configures native lsp and integrates nvim-cmp and mason to apply language servers ]]
 -- Define function that will be called whenever a language server attatches to a buffer
+local lsp = vim.lsp
+
 local on_attach = function(_, bufnr)
   local nmap = function(keys, func, desc)
     if desc then
@@ -9,28 +11,28 @@ local on_attach = function(_, bufnr)
     vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
   end
 
-  nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-  nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+  nmap("<leader>rn", lsp.buf.rename, "[R]e[n]ame")
+  nmap("<leader>ca", lsp.buf.code_action, "[C]ode [A]ction")
 
-  nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-  nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-  nmap("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-  nmap("gt", vim.lsp.buf.type_definition, "[G]oto [T]ype Definition")
+  nmap("gd", lsp.buf.definition, "[G]oto [D]efinition")
+  nmap("gD", lsp.buf.declaration, "[G]oto [D]eclaration")
+  nmap("gi", lsp.buf.implementation, "[G]oto [I]mplementation")
+  nmap("gt", lsp.buf.type_definition, "[G]oto [T]ype Definition")
 
   nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
   nmap("gs", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[G]oto Workspace [S]ymbols")
 
-  nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-  nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+  nmap("K", lsp.buf.hover, "Hover Documentation")
+  nmap("<C-k>", lsp.buf.signature_help, "Signature Documentation")
 
-  nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-  nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+  nmap("<leader>wa", lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+  nmap("<leader>wr", lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
   nmap("<leader>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    print(vim.inspect(lsp.buf.list_workspace_folders()))
   end, "[W]orkspace [L]ist Folders")
 
   vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-    vim.lsp.buf.format()
+    lsp.buf.format()
   end, { desc = "Format current buffer with LSP" })
 
   -- Automatically display diagnostics when hovering over an error
@@ -59,7 +61,11 @@ local on_attach = function(_, bufnr)
     end,
   })
 
-
+  -- Add border to floating windows, get the border style from the theme file
+  require("lspconfig.ui.windows").default_options.border = require("yellowbeans.styles").border
+  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    border = "single"
+  })
 end
 
 -- List of configured language servers
@@ -121,6 +127,3 @@ require("mason-lspconfig").setup_handlers {
     }
   end,
 }
-
--- Add border to floating windows, get the border style from the theme file
-require("lspconfig.ui.windows").default_options.border = require("yellowbeans.styles").border
