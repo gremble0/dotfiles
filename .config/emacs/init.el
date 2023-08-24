e1c0fffd58a8489782cc3602022b5ea8a69e02cd
gremble0 <hermanstornes@gmail.com>
2023-08-24 09:47:22 +0200

    Replaced elpaca with default use-package, fixed tree-sitter (mostly)

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,39 +1,54 @@
+(require 'package)
+(setq package-user-dir "~/.config/emacs/packages"
+      use-package-always-ensure t)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(use-package vertico
+  :ensure t
+  :init (vertico-mode))
+
 ;; Install packages
 ;; Show hints for keybinds
 (use-package which-key
+  :ensure t
   :init
   (which-key-mode)
   :init
   (setq which-key-side-window-location 'bottom
-	which-key-sort-order #'which-key-key-order-alpha
-	which-key-sort-uppercase-first nil
-	which-key-add-column-padding 1
-	which-key-max-display-columns nil
-	which-key-min-display-lines 6
-	which-key-side-window-slot -10
-	which-key-side-window-max-height 0.25
-	which-key-idle-delay 0.8
-	which-key-max-description-length 25
-	which-key-allow-imprecise-window-fit t
-	which-key-separator " → " ))
+ 	which-key-sort-order #'which-key-key-order-alpha
+ 	which-key-sort-uppercase-first nil
+ 	which-key-add-column-padding 1
+ 	which-key-max-display-columns nil
+ 	which-key-min-display-lines 6
+ 	which-key-side-window-slot -10
+ 	which-key-side-window-max-height 0.25
+ 	which-key-idle-delay 0.8
+ 	which-key-max-description-length 25
+ 	which-key-allow-imprecise-window-fit t
+ 	which-key-separator " → " ))
 
 ;; Evil mode
 (use-package evil
+  :ensure t
   :init
   (setq evil-want-keybinding nil
-	evil-want-C-u-scroll t
-	evil-vsplit-window-below t
-	evil-want-fine-undo 'fine
-	evil-undo-system 'undo-redo)
+ 	evil-want-C-u-scroll t
+ 	evil-vsplit-window-below t
+ 	evil-want-fine-undo 'fine
+ 	evil-undo-system 'undo-redo)
   (evil-mode))
 
 (use-package evil-collection
+  :ensure t
   :after evil
   :init
   (setq evil-collection-setup-minibuffer t)
   (evil-collection-init))
 
 (use-package general
+  :ensure t
   :init
   (general-evil-setup)
 
@@ -46,8 +61,11 @@
     "C-c C-k" 'kill-buffer-and-window
     "C-c C-e" 'eval-buffer
 
-    ;; Help commands
-    "C-h F" 'describe-face)
+    ;; Misc
+    "C-c C-t" 'vterm-toggle
+    "C-h F" 'describe-face
+    "M-r" 'shell-command
+    "C-s" 'consult-line)
 
   ;; Keybinds for specific vim modes
   (general-def 'normal
@@ -77,24 +95,27 @@
 
 ;; Terminal
 (use-package eshell-syntax-highlighting
+  :ensure t
   :after esh-mode
   :init
   (eshell-syntax-highlighting-global-mode))
 
 (use-package vterm
+  :ensure t
   :init
   (setq shell-file-name "/bin/zsh"
 	vterm-max-scrollback 10000))
 
 (use-package vterm-toggle
+  :ensure t
   :after vterm
-  :bind (("C-c C-v" . 'vterm-toggle))
   :init
   (setq vterm-toggle-fullscreen-p nil)
   (setq vterm-toggle-scope 'project))
 
 ;; Completion
 (use-package vertico
+  :ensure t
   :bind (:map minibuffer-local-map
 	      ("C-<backspace>" . (lambda (arg)
 				   (interactive "p")
@@ -107,18 +128,21 @@
   (vertico-mode))
 
 (use-package consult
-  :bind (("C-s" . consult-line)))
+  :ensure t)
 
 (use-package marginalia
+  :ensure t
   :init
   (setq marginalia-align 'right)
   (marginalia-mode))
 
 (use-package orderless
+  :ensure t
   :init
   (setq completion-styles '(orderless flex)))
 
 (use-package corfu
+  :ensure t
   :init
   (setq corfu-auto t
 	corfu-quit-no-match 'separator)
@@ -126,11 +150,13 @@
 
 ;; Mode-Line
 (use-package mood-line
+  :ensure t
   :init
   (mood-line-mode))
 
 ;; See lines as indentation guides
 (use-package highlight-indent-guides
+  :ensure t
   :init
   (setq highlight-indent-guides-method 'character)
   (setq highlight-indent-guides-auto-character-face-perc 100)
@@ -138,12 +164,15 @@
 
 ;; Preview colors while editing
 (use-package rainbow-mode
+  :ensure t
   :hook org-mode prog-mode help-mode)
 
 ;; Git
 (use-package magit)
+:ensure t
 
 (use-package git-gutter
+  :ensure t
   :hook (prog-mode . git-gutter-mode)
   :bind (("C-c gn" . 'git-gutter:next-hunk)
 	 ("C-c gp" . 'git-gutter:previous-hunk))
@@ -152,37 +181,35 @@
 
 ;; Language support
 (use-package eros
+  :ensure t
   :init
   (eros-mode))
 
-(use-package lua-mode)
+(use-package lua-mode
+  :ensure t)
 
 (use-package geiser
+  :ensure t
   :init
   (setq geiser-active-implementations '(racket)))
 
 (use-package geiser-chez
+  :ensure t
   :after geiser)
 
 (use-package lsp-mode
+  :ensure t
   :init
   (setq lsp-keymap-prefix "C-l"))
 
-(add-to-list 'load-path "~/.config/emacs/elpaca/repos/elisp-tree-sitter/core")
-(add-to-list 'load-path "~/.config/emacs/elpaca/repos/elisp-tree-sitter/lisp")
-(add-to-list 'load-path "~/.config/emacs/elpaca/repos/tree-sitter-langs")
-(require 'tree-sitter)
-(require 'tree-sitter-hl)
-(require 'tree-sitter-langs)
-(require 'tree-sitter-debug)
-(require 'tree-sitter-query)
+(use-package tree-sitter
+  :ensure t
+  :init
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
 
-;; (use-package tree-sitter
-;;   :init
-;;   (global-tree-sitter-mode)
-;;   (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
-
-;; (use-package tree-sitter-langs)
+(use-package tree-sitter-langs
+  :ensure t)
 
 (use-package lsp-pyright
   :hook (python-mode . (lambda ()
@@ -228,19 +255,13 @@
       dired-listing-switches "-AhgGoF --group-directories-first --color=auto"
 
       backup-directory-alist '((".*" . "~/.cache/emacs")))
-
-;; Autogenerated
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("6558fa279269fa352947bf310f7dc2b6e25745d539c9974628e569a3dbff78db" "68483b0eb8ad569b1212b1e160ff920ac98dc7cf67668a0eb53ffac632778300" "9b7cae793c4e8bbbdab743fa38f857176fb09f77ca0690d8aa960e40b3900109" "30af43ce54f038a9c1c60893a7100d40ef5e7e86d307a1b5c338f768708c32f9" "93bb13b355ca389a2929fc14bb27b8f864234ef3e444728871037d2d6e5c1299" "ee92cd6b037bbc6e496cdcca1e306c4c864ff6ee712c2fad33916a33b8bdb005" "52487739a5864f2c022ddf861c9c7cedad9c028ac61145a8a6447c6784ad4b9c" default))
- '(highlight-indent-guides-auto-even-face-perc 100)
- '(highlight-indent-guides-auto-odd-face-perc 100)
  '(package-selected-packages
-   '(geiser-racket vertico magit orderless git-gutter general evil which-key)))
+   '(which-key vterm-toggle vertico rainbow-mode orderless mood-line marginalia magit lua-mode lsp-mode highlight-indent-guides git-gutter general geiser-chez evil-collection eshell-syntax-highlighting eros corfu consult)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
