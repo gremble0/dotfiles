ab6348cb5fd6e44161b5c0cf60aaeec55bed3ef4
gremble0 <hermanstornes@gmail.com>
2023-11-21 10:06:31 +0100

    emacs-like keybinds for neogit

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -2,8 +2,14 @@
 local neogit = require("neogit")
 
 neogit.setup {
-  vim.keymap.set("n", "<leader>gt", neogit.open, { desc = "Toggle nvim-tree", silent = true }),
+  vim.keymap.set("n", "<leader>gt", neogit.open, { desc = "Open neogit" }),
 
   disable_hint = true,
   kind = "vsplit",
+  mappings = {
+    popup = {
+      ["p"] = "PushPopup",
+      ["P"] = "PullPopup",
+    }
+  }
 }
