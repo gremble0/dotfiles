646de99fc108467bc449919db081ee4250c8ddeb
gremble0 <hermanstornes@gmail.com>
2023-12-17 07:14:44 +0100

    Listen to more appropriate auto command

--- .config/nvim/lua/bufferstack/init.lua
+++ .config/nvim/lua/bufferstack/init.lua
@@ -17,19 +17,29 @@ function M.add_buffer(buffer)
   M.buffers_index = #M.buffers
 end
 
-function M.garbage_collect()
-  for i, buf in ipairs(M.buffers) do
-    if not vim.api.nvim_buf_is_loaded(buf) then
-      for j = i, #M.buffers do
-        M.buffers[j] = M.buffers[j + 1]
-      end
-    end
+-- function M.garbage_collect()
+--   for i, buf in ipairs(M.buffers) do
+--     if not vim.api.nvim_buf_is_loaded(buf) then
+--       for j = i, #M.buffers do
+--         M.buffers[j] = M.buffers[j + 1]
+--       end
+--     end
+--   end
+-- end
+
+function M.bnext()
+  if M.buffers_index == #M.buffers then
+    M.buffers_index = 1
+  else
+    M.buffers_index = M.buffers_index + 1
   end
+
+  vim.api.nvim_set_current_buf(M.buffers[M.buffers_index])
+
+  M.show()
 end
 
 function M.bprevious()
-  M.garbage_collect()
-
   if M.buffers_index == 1 then
     M.buffers_index = #M.buffers
   else
@@ -37,20 +47,8 @@ function M.bprevious()
   end
 
   vim.api.nvim_set_current_buf(M.buffers[M.buffers_index])
-end
 
-function M.bnext()
-  M.garbage_collect()
-
-  if M.buffers_index == #M.buffers then
-    M.buffers_index = 1
-  else
-    M.buffers_index = M.buffers_index + 1
-  end
-
-  print(M.buffers_index)
-
-  vim.api.nvim_set_current_buf(M.buffers[M.buffers_index])
+  M.show()
 end
 
 function M.show()
@@ -70,7 +68,10 @@ function M.setup()
   -- TODO autocmd group
   M.buffers = {}
   M.buffers_index = 1
-  vim.api.nvim_create_autocmd("BufEnter", {
+
+  local buffer_stack_group = vim.api.nvim_create_augroup("BufferStack", {})
+  vim.api.nvim_create_autocmd("BufWinEnter", {
+    group = buffer_stack_group,
     callback = function() M.add_buffer(vim.api.nvim_get_current_buf()) end
   })
 end
