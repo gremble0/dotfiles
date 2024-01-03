d05c5936fcf82357b11d453e036eb63b84f4d9d7
gremble0 <hermanstornes@gmail.com>
2024-01-03 21:05:05 +0100

    Better keybinds for cmp and luasnip

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,24 +1,15 @@
--- [[ Configure nvim-cmp ]]
+-- [[ Configure nvim-cmp and luasnip ]]
 local cmp = require("cmp")
 local lspkind = require("lspkind")
 local luasnip = require("luasnip")
 require("luasnip.loaders.from_vscode").lazy_load()
 luasnip.config.setup()
 
-vim.keymap.set({ "i", "s", "n" }, "<C-/>", function() luasnip.expand() end, { silent = true })
-vim.keymap.set({ "i", "s", "n" }, "<C-;>", function() luasnip.jump(1) end, { silent = true })
-vim.keymap.set({ "i", "s", "n" }, "<C-,>", function() luasnip.jump(-1) end, { silent = true })
-
-local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 local cmpborder = { "┌",  "─",  "┐",  "│",  "┘",  "─",  "└",  "│" }
 
 cmp.setup {
   formatting = {
+    expandable_indicator = false,
     fields = { "kind", "abbr", "menu" },
     format = function(entry, vim_item)
       local kind = lspkind.cmp_format({ mode = "symbol_text", maxwidth = 35 })(entry, vim_item)
@@ -35,24 +26,36 @@ cmp.setup {
     end,
   },
   mapping = cmp.mapping.preset.insert {
-    ["<C-n>"] = cmp.mapping(function(fallback)
+    ["<C-n>"] = cmp.mapping(function(_)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
       else
-        fallback()
+        cmp.complete()
       end
     end, { "i", "s" }),
-    ["<C-p>"] = cmp.mapping(function(fallback)
+    ["<C-p>"] = cmp.mapping(function(_)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
         luasnip.jump(-1)
       else
-        fallback()
+        cmp.complete()
+      end
+    end, { "i", "s" }),
+    ["<Tab>"] = cmp.mapping(function(callback)
+      if luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        callback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(callback)
+      if luasnip.expand_or_jumpable() then
+        luasnip.jump(-1)
+      else
+        callback()
       end
     end, { "i", "s" }),
     ["<C-u>"] = cmp.mapping.scroll_docs(-4),
@@ -65,7 +68,7 @@ cmp.setup {
   },
   window = {
     completion = cmp.config.window.bordered({
-      border = cmpborder,
+      border = cmpborder, -- "rounded"
       side_padding = 0,
       col_offset = -3,
       winhighlight = "Normal:Pmenu,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
@@ -73,7 +76,7 @@ cmp.setup {
       scrolloff = 2,
     }),
     documentation = cmp.config.window.bordered({
-      border = cmpborder,
+      border = cmpborder, -- "rounded"
       side_padding = 0,
       winhighlight = "Normal:Pmenu,FloatBorder:FloatBorder,Search:None",
       scrolloff = 2,
