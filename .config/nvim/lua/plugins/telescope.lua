8940e845ff2df27f383d310dfdd8adcd593cd7a6
gremble0 <hermanstornes@gmail.com>
2024-04-06 00:12:06 +0200

    Try dressing.nvim

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -11,8 +11,6 @@ return {
         return vim.fn.executable("make") == 1
       end,
     },
-
-    "nvim-telescope/telescope-ui-select.nvim",
   },
 
   config = function()
@@ -72,13 +70,9 @@ return {
           mappings = { i = { ["<C-CR>"] = tabnew } },
         },
       },
-      extensions = {
-        ["ui-select"] = { require("telescope.themes").get_cursor() },
-      },
     })
 
     telescope.load_extension("fzf")
-    telescope.load_extension("ui-select")
   end,
 
   keys = {
@@ -89,6 +83,4 @@ return {
     { "<leader>gh", ":Telescope help_tags<CR>", desc = "Telescope find help", silent = true },
     { "<leader>gr", ":Telescope live_grep<CR>", desc = "Telescope live grep", silent = true },
   },
-
-  lazy = false,
 }
