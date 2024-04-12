55a1e0d0d6f61e5d920b2752b0bf6625fa7011ad
gremble0 <hermanstornes@gmail.com>
2024-04-12 22:58:16 +0200

    Update fugitive binds

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -17,10 +17,9 @@ return {
       pattern = "fugitive",
       callback = function(e)
         vim.keymap.del("n", "p", { buffer = e.buf })
-        vim.keymap.del("n", "P", { buffer = e.buf })
 
         vim.keymap.set("n", "pu", ":Git push<CR>", { desc = "Git push", silent = true, buffer = e.buf })
-        vim.keymap.set("n", "Pu", ":Git pull<CR>", { desc = "Git pull", silent = true, buffer = e.buf })
+        vim.keymap.set("n", "pl", ":Git pull<CR>", { desc = "Git pull", silent = true, buffer = e.buf })
       end,
     })
   end,
