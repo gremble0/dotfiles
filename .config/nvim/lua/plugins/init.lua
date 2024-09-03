0ef8c2eac476562b3741745524e0ac210c788a68
gremble0 <hermanstornes@gmail.com>
2024-09-03 09:54:16 +0200

    Add vim-dadbod to nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,6 +1,12 @@
 -- Plugins that don't require any setup
 return {
   -- Adjusts indentation based on what the file is using
-  "tpope/vim-sleuth",
   "folke/tokyonight.nvim",
+
+  "tpope/vim-sleuth",
+
+  {
+    "tpope/vim-dadbod",
+    dependencies = "kristijanhusak/vim-dadbod-ui",
+  },
 }
