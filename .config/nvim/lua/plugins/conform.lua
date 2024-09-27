9f58281431df230fbc0ea5d756fcd36da11e5534
gremble0 <hermanstornes@gmail.com>
2024-09-27 12:56:32 +0200

    Fix out of line opts for conform

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -32,23 +32,12 @@ return {
       formatters_by_ft = {
         lua = { "stylua" },
         javascript = { "prettierd" },
+        javascriptreact = { "prettierd" },
+        typescript = { "prettierd" },
+        typescriptreact = { "prettierd" },
       },
     })
   end,
-
-  opts = {
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-    formatters_by_ft = {
-      lua = { "stylua" },
-      javascript = { "prettierd" },
-      javascriptreact = { "prettierd" },
-      typescript = { "prettierd" },
-      typescriptreact = { "prettierd" },
-    },
-  },
 
   -- Load plugin when writing to file/<leader>mt key is pressed/:ConformInfo command is executed
   event = "BufWritePost",
