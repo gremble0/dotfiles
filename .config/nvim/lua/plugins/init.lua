660dc69589cd6bc71cb15af6eda25c0a16c625f6
gremble0 <hermanstornes@gmail.com>
2024-10-12 12:34:01 +0200

    Lazy load vim-dadbod

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,12 +1,6 @@
 -- Plugins that don't require any setup
 return {
-  -- Adjusts indentation based on what the file is using
   "folke/tokyonight.nvim",
 
   "tpope/vim-sleuth",
-
-  {
-    "tpope/vim-dadbod",
-    dependencies = "kristijanhusak/vim-dadbod-ui",
-  },
 }
