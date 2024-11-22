7dc7dfc8f2a245504fb04fb7a9a665c5b3926d8b
gremble0 <hermanstornes@gmail.com>
2024-11-22 23:30:37 +0100

    Fix conform not formatting on first BufWritePost

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,49 +2,49 @@
 return {
   "stevearc/conform.nvim",
 
-  config = function()
-    vim.api.nvim_create_user_command("ConformDisable", function(args)
-      if args.bang then
-        vim.b.disable_autoformat = true
-      else
-        vim.g.disable_autoformat = true
-      end
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = { "prettierd" },
+      javascriptreact = { "prettierd" },
+      typescript = { "prettierd" },
+      typescriptreact = { "prettierd" },
+    },
+  },
+
+  config = function(_, opts)
+    vim.api.nvim_create_user_command("ConformDisable", function()
+      vim.g.disable_autoformat = true
     end, {
       desc = "Disable autoformat-on-save",
-      bang = true,
     })
 
     vim.api.nvim_create_user_command("ConformEnable", function()
-      vim.b.disable_autoformat = false
       vim.g.disable_autoformat = false
     end, {
       desc = "Re-enable autoformat-on-save",
     })
 
-    require("conform").setup({
-      format_on_save = function(bufnr)
-        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-          return
-        end
-        return { timeout_ms = 500, lsp_format = "fallback" }
-      end,
-
-      formatters_by_ft = {
-        lua = { "stylua" },
-        javascript = { "prettierd" },
-        javascriptreact = { "prettierd" },
-        typescript = { "prettierd" },
-        typescriptreact = { "prettierd" },
-      },
-    })
+    require("conform").setup(opts)
   end,
 
-  -- Load plugin when writing to file/<leader>mt key is pressed/:ConformInfo command is executed
-  event = "BufWritePost",
+  -- Only setting BufWritePost as event will not also format the buffer first time the
+  -- autocommand is executed. This initializes the autocommand for formatting before the
+  -- plugin is loaded which means it will also format on the first BufWritePost event
+  event = function(_, _)
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      group = vim.api.nvim_create_augroup("Conform", {}),
+      callback = function()
+        require("conform").format({ timeout_ms = 1000, lsp_format = "fallback" })
+      end,
+    })
+
+    return { "BufWritePost" }
+  end,
 
   keys = {
     { "<leader>mt", ":lua require('conform').format()<CR>", desc = "Format current buffer", silent = true },
   },
 
-  cmd = "ConformInfo",
+  cmd = { "ConformInfo", "ConformEnable" },
 }
