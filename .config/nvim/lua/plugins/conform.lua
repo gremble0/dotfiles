4b211bfa61746bf6156eea349f5112ebd4d80600
gremble0 <hermanstornes@gmail.com>
2025-01-04 01:40:16 +0100

    Remove unnecessary check inside autoformat autocmd

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -11,24 +11,24 @@ return {
     },
   },
   config = function(_, opts)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = vim.api.nvim_create_augroup("ConformAutoFormat", { clear = false }),
-      pattern = "*",
-      callback = function()
-        if vim.g.enable_autoformat then
-          require("conform").format({ timeout_ms = 1000, lsp_format = "fallback" })
-        end
-      end,
-    })
+    local autoformat_cmd = nil
 
     vim.api.nvim_create_user_command("ConformDisable", function()
-      vim.g.enable_autoformat = false
+      if autoformat_cmd then
+        vim.api.nvim_del_autocmd(autoformat_cmd)
+      end
     end, {
       desc = "Disable autoformat-on-save",
     })
 
     vim.api.nvim_create_user_command("ConformEnable", function()
-      vim.g.enable_autoformat = true
+      autoformat_cmd = vim.api.nvim_create_autocmd("BufWritePre", {
+        group = vim.api.nvim_create_augroup("ConformAutoFormat", { clear = false }),
+        pattern = "*",
+        callback = function()
+          require("conform").format({ timeout_ms = 1000, lsp_format = "fallback" })
+        end,
+      })
     end, {
       desc = "Re-enable autoformat-on-save",
     })
