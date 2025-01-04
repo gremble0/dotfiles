d43f1203a328480ab882a16851fb9c0e8fd36949
gremble0 <hermanstornes@gmail.com>
2025-01-04 01:33:41 +0100

    Fix autoformat, and format on key conform

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -14,21 +14,21 @@ return {
     vim.api.nvim_create_autocmd("BufWritePre", {
       group = vim.api.nvim_create_augroup("ConformAutoFormat", { clear = false }),
       pattern = "*",
-      callback = function(args)
-        if not vim.g.disable_autoformat then
-          require("conform").format({ bufnr = args.buf, timeout_ms = 1000, lsp_format = "fallback" })
+      callback = function()
+        if vim.g.enable_autoformat then
+          require("conform").format({ timeout_ms = 1000, lsp_format = "fallback" })
         end
       end,
     })
 
     vim.api.nvim_create_user_command("ConformDisable", function()
-      vim.g.disable_autoformat = true
+      vim.g.enable_autoformat = false
     end, {
       desc = "Disable autoformat-on-save",
     })
 
     vim.api.nvim_create_user_command("ConformEnable", function()
-      vim.g.disable_autoformat = false
+      vim.g.enable_autoformat = true
     end, {
       desc = "Re-enable autoformat-on-save",
     })
@@ -36,7 +36,14 @@ return {
     require("conform").setup(opts)
   end,
   keys = {
-    { "<leader>mt", ":lua require('conform').format()<CR>", desc = "Format current buffer", silent = true },
+    {
+      "<leader>mt",
+      ":lua require('conform').format({ timeout_ms = 1000, lsp_format = 'fallback' })<CR>",
+      desc = "Format current buffer",
+      silent = true,
+    },
+    { "<leader>me", ":ConformEnable<CR>", desc = "Enable autoformatting", silent = true },
+    { "<leader>md", ":ConformDisable<CR>", desc = "Disable autoformatting", silent = true },
   },
   cmd = { "ConformInfo", "ConformEnable", "ConformDisable" },
 }
