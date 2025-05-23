c3ddf81be4057e9de4c147bd94233742f9612d9c
gremble0 <hermanstornes@gmail.com>
2025-05-23 16:26:14 +0200

    Fix capitalized usercmds

--- .config/nvim/lua/core/usercmd.lua
+++ .config/nvim/lua/core/usercmd.lua
@@ -1,7 +1,11 @@
 vim.api.nvim_create_user_command("W", function(opts)
-  vim.cmd("w" .. opts.bang and "!" or "")
+  vim.cmd("w" .. (opts.bang and "!" or ""))
 end, { bang = true })
 
 vim.api.nvim_create_user_command("E", function(opts)
-  vim.cmd("e" .. opts.bang and "!" or "")
+  vim.cmd("e" .. (opts.bang and "!" or ""))
+end, { bang = true })
+
+vim.api.nvim_create_user_command("Q", function(opts)
+  vim.cmd("q" .. (opts.bang and "!" or ""))
 end, { bang = true })
