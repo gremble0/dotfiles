297e9928664989448a6fb4ddc1891a45f3e194ea
gremble0 <hermanstornes@gmail.com>
2025-07-10 20:58:18 +0200

    Remove snippets nvim

--- .config/nvim/lua/snippets/c.lua
+++
@@ -1,9 +0,0 @@
-local ls = require("luasnip")
-
-local s = ls.snippet
-local i = ls.insert_node
-local fmt = require("luasnip.extras.fmt").fmt
-
-ls.add_snippets("c", {
-  s("fori", fmt("for ({}; {}; {})", { i(1, "int i = 0"), i(2, "i < n"), i(3, "++i") })),
-})
