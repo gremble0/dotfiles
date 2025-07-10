aeef291e97d563457e47d9c32fdcdb4a1ed34409
gremble0 <hermanstornes@gmail.com>
2025-07-10 20:23:59 +0200

    Generate all case combinations of wq, wqa, etc. user commands

--- .config/nvim/lua/core/usercmd.lua
+++ .config/nvim/lua/core/usercmd.lua
@@ -1,11 +1,34 @@
-vim.api.nvim_create_user_command("W", function(opts)
-  vim.cmd("w" .. (opts.bang and "!" or ""))
-end, { bang = true })
+---NOTE: Do not call this function with longer strings as it will generate a LOT of combinations
+---scaling exponentially with the length of the string (O(2 ^ n))
+---@param user_command string
+local function generate_case_combinations(user_command)
+  ---@type string[]
+  local combinations = {}
 
-vim.api.nvim_create_user_command("E", function(opts)
-  vim.cmd("e" .. (opts.bang and "!" or ""))
-end, { bang = true })
+  for i = 0, (2 ^ #user_command) - 1 do
+    -- User commands must start with a capital letter. Only generate combinations for letters after the first.
+    local combination = user_command:sub(1, 1):upper()
+    for j = 2, #user_command do
+      local bit = math.floor(i / (2 ^ (j - 1))) % 2
+      if bit == 1 then
+        combination = combination .. user_command:sub(j, j):upper()
+      else
+        combination = combination .. user_command:sub(j, j):lower()
+      end
+    end
+    table.insert(combinations, combination)
+  end
 
-vim.api.nvim_create_user_command("Q", function(opts)
-  vim.cmd("q" .. (opts.bang and "!" or ""))
-end, { bang = true })
+  return combinations
+end
+
+-- Make usercommands for all possible case combinations of these commonly used commands
+local commands = { "w", "wq", "wqa", "q", "qa", "e" }
+
+for _, command in ipairs(commands) do
+  for _, combo in ipairs(generate_case_combinations(command)) do
+    vim.api.nvim_create_user_command(combo, function(opts)
+      vim.cmd(command .. (opts.bang and "!" or ""))
+    end, { bang = true })
+  end
+end
